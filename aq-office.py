b6d1fc4db6e02b6d6e56d6bf921f40c39c08e430
JFG <jgailleur@hotmail.com>
2017-03-03 13:18:17 -0500

    initial commit, air quality at my desk!

--- aq-office.py
+++ aq-office.py
@@ -50,10 +50,19 @@ import grove_sensor_oo_lib
 # Connect the CO2 sensor to the RPISER port on the GrovePi
 import grove_co2_lib
 
+import dweet_io_lib
 
 #----------------------------------
 # MAIN PROGRAM
 #----------------------------------
+#if __name__ == "__main__":
+
+# CONFIGURATION
+
+stream_dweet_io = True # dweet streaming
+stream_online = False # Initial state streaming
+
+
 # INITIALIZATION
 
 #- STOCKAGE SUR INTERNET
@@ -63,7 +72,7 @@ BUCKET_KEY_AQ = "20070301-EV"
 # intial state access key for jgailleur@hotmail.com
 ACCESS_KEY = "0Vcs79QnlzNa7tO7Bn1sJ0LHgzyuTJaj"
 
-stream_online = False
+
 
 
 # Set the time between sensor reads
@@ -87,6 +96,11 @@ if (stream_online):
     streamer_aq = Streamer(bucket_name=BUCKET_NAME_AQ, bucket_key=BUCKET_KEY_AQ, access_key=ACCESS_KEY)
 
 
+
+# dweet.io
+if (stream_dweet_io):
+    dweet = dweet_io_lib.Dweet()
+
 # infinite loop
 while True:
     
@@ -106,6 +120,11 @@ while True:
         air_quality_sensor_value = air_quality_sensor.readAirQuality()
         air_type_string = air_quality_sensor.getAirQualityStringValue(air_quality_sensor_value)
         print("Air quality (1-900), lower better: "+str(air_type_string)+" (%d)" %(air_quality_sensor_value))
+
+
+
+        #dweet with a thing name
+        print (dweet.dweet_by_name(name="office-20170303", data={"airquality": str(air_quality_sensor_value)}))
 
 
         #------------------------------------------------------
